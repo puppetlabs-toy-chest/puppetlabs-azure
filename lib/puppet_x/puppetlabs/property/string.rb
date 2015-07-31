0d51605fd4f80d9364f2eda9c96cecc7425b05e5
Gareth Rushgrove <gareth@morethanseven.net>
2015-07-31 12:34:12 +0100

    (CLOUD-408) Define the Azure VM type

---
+++ lib/puppet_x/puppetlabs/property/string.rb
@@ -0,0 +1,9 @@
+module PuppetX
+  module Property
+    class String < Puppet::Property
+      validate do |value|
+        fail "#{self.name.to_s} should be a String" unless value.is_a? ::String
+      end
+    end
+  end
+end
