9ad110d6b4f78ed7825007bc511d04fe7e009269
Greg Hardy <greg.hardy@puppetlabs.com>
2015-09-29 17:22:37 +0100

    (CLOUD-438) Support the resource management API in Azure
    Azure currently has two APIs; service management (or classic) and
    resource management (or ARM for short). This commit adds initial support
    for the resource management API in the form of the azure_vm type.
    
    Specifically we support a pretty low level interface to a wide range of
    properties for the VM, as well as the ability to create, stop, restart
    and destroy machines. The type does not yet support any form of
    mutation.

---
+++ spec/acceptance/arm_vm_spec.rb
@@ -0,0 +1,125 @@
+require 'spec_helper_acceptance'
+
+describe 'azure_vm when creating a machine with all available properties' do
+  include_context 'with a known name and storage account name'
+  include_context 'destroy left-over created ARM resources after use'
+
+  before(:all) do
+    @config = {
+      name: @name,
+      ensure: 'present',
+      optional: {
+        image: 'canonical:ubuntuserver:14.04.2-LTS:latest',
+        location: 'eastus',
+        user: 'specuser',
+        size: 'Standard_A0',
+        resource_group: 'puppettestresacc02',
+        password: 'SpecPass123!@#$%',
+        storage_account: @storage_account_name,
+        storage_account_type: 'Standard_GRS',
+        os_disk_name: 'osdisk01',
+        os_disk_caching: 'ReadWrite',
+        os_disk_create_option: 'FromImage',
+        os_disk_vhd_container_name: 'conttest1',
+        os_disk_vhd_name: 'vhdtest1',
+        dns_domain_name: 'mydomain01',
+        dns_servers: '10.1.1.1 10.1.2.4',
+        public_ip_allocation_method: 'Dynamic',
+        public_ip_address_name: 'ip_name_test01pubip',
+        virtual_network_name: 'vnettest01',
+        virtual_network_address_space: '10.0.0.0/16',
+        subnet_name: 'subnet111',
+        subnet_address_prefix: '10.0.2.0/24',
+        ip_configuration_name: 'ip_config_test01',
+        private_ipallocation_method: 'Dynamic',
+        network_interface_name: 'nicspec01',
+      },
+    }
+    @template = 'azure_vm.pp.tmpl'
+    @client = AzureARMHelper.new
+    @manifest = PuppetManifest.new(@template, @config)
+    @result = @manifest.execute
+    @machine = @client.get_vm(@name)
+  end
+
+  it_behaves_like 'an idempotent resource'
+
+  it 'should have the correct name' do
+    expect(@machine.name).to eq(@name)
+  end
+
+  it 'should have the correct size' do
+    expect(@machine.properties.hardware_profile.vm_size).to eq(@config[:optional][:size])
+  end
+
+  it 'should be running' do
+    expect(@client.vm_running?(@machine)).to be true
+  end
+
+  context 'when puppet resource is run' do
+    include_context 'a puppet ARM resource run'
+    puppet_resource_should_show('ensure', 'running')
+    puppet_resource_should_show('location', 'eastus')
+    puppet_resource_should_show('image')
+    puppet_resource_should_show('user')
+    puppet_resource_should_show('size')
+    puppet_resource_should_show('resource_group')
+  end
+
+  context 'when we try and stop the VM' do
+    before(:all) do
+      new_config = @config.update({:ensure => 'stopped'})
+      @manifest = PuppetManifest.new(@template, new_config)
+      @result = @manifest.execute
+      @machine = @client.get_vm(@name)
+    end
+
+    it_behaves_like 'an idempotent resource'
+
+    it 'should be stopped' do
+      expect(@client.vm_stopped?(@machine)).to be true
+    end
+
+    context 'when looked for using puppet resource' do
+      include_context 'a puppet ARM resource run'
+      puppet_resource_should_show('ensure', 'stopped')
+    end
+
+    context 'when we try and restart the VM' do
+      before(:all) do
+        new_config = @config.update({:ensure => 'running'})
+        @manifest = PuppetManifest.new(@template, new_config)
+        @result = @manifest.execute
+        @machine = @client.get_vm(@name)
+      end
+
+      it_behaves_like 'an idempotent resource'
+
+      it 'should be running' do
+        expect(@client.vm_running?(@machine)).to be true
+      end
+
+      context 'when looked for using puppet resource' do
+        include_context 'a puppet ARM resource run'
+        puppet_resource_should_show('ensure', 'running')
+      end
+    end
+  end
+
+  context 'when we try and destroy the VM' do
+    before(:all) do
+      new_config = @config.update({:ensure => 'absent'})
+      manifest = PuppetManifest.new(@template, new_config)
+      @result = manifest.execute
+      @machine = @client.get_vm(@name)
+    end
+
+    it 'should run without errors' do
+      expect(@result.exit_code).to eq 2
+    end
+
+    it 'should be destroyed' do
+      expect(@machine).to be_nil
+    end
+  end
+end
--- spec/support/context/destroy_leftover_resources.rb
+++ spec/support/context/destroy_leftover_resources.rb
@@ -10,3 +10,17 @@ shared_context 'destroy left-over created resources after use' do
     @client.destroy_storage_account(@storage_account_name) if @client.get_storage_account(@storage_account_name)
   end
 end
+
+shared_context 'destroy left-over created ARM resources after use' do
+  after(:all) do
+    # Some tests remove the VM themselves. This check avoids a error message in that case
+    @client.destroy_vm(@machine) unless @client.get_vm(@machine).nil?
+    @client.destroy_resource_group(
+      @config[:optional][:resource_group]
+    ) if @client.get_resource_group(@config[:optional][:resource_group])
+    @client.destroy_storage_account(
+      @config[:optional][:resource_group],
+      @config[:optional][:storage_account]
+    ) if @client.get_storage_account(@config[:optional][:storage_account])
+  end
+end
